6beb77d3e2b2e1b5f052fd2c64301723a78f7e7b
balaji <balaji78@gmail.com>
2024-02-02 13:15:21 +0530

    express files added

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const adminRouter = require("./routes/admin")
+const shopRouter = require("./routes/shop")
+const path = require("path");
+
+
+
+app.use("/admin",adminRouter);
+app.use(shopRouter);
+
+app.use((req, res, next)=>{
+    res.status(404).sendFile(path.join(__dirname,"views","error.html"))
+})
+
+
+app.use(bodyParser.urlencoded({ extended: true }))    
+
+ app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
+const path = require("path");
+const rootDir = require("../utils/path")
+
+router.use(bodyParser.urlencoded({ extended: true }))    
+ 
+
+router.get("/add-product",(req,res,next)=>{
+    res.sendFile(path.join(rootDir,"views","addProduct.html"))
+ })
+
+router.post("/add-product",(req,res,next)=>{
+    console.log("Form data : " , req.body)
+    res.send("product send successfully")
+ })
+
+ module.exports = router;
